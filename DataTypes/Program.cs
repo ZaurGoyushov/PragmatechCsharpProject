da9395257f6df65d8c17c5fb41a975f09608464d
parvizrovshanaliyev <parvizra@code.edu.az>
2021-03-14 13:17:33 +0400

    14.03.2021 week 2 day 2 typeConversion

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Cache;
 
 namespace DataTypes
 {
@@ -6,7 +7,7 @@ namespace DataTypes
     {
         static void Main(string[] args)
         {
-
+            
             #region int  byte double float decimal
 
             //int intMinVal, a, b;
@@ -38,13 +39,13 @@ namespace DataTypes
 
             #region string
 
-            string groupName = "CSharp-02";
-            string courseName = "Pragmatech";
-            string email = "kanan@pragmatech.az";
+            //string groupName = "CSharp-02";
+            //string courseName = "Pragmatech";
+            //string email = "kanan@pragmatech.az";
 
-            Console.WriteLine(groupName.ToLower());
-            Console.WriteLine(groupName.ToUpper());
-            Console.WriteLine(groupName.Substring(0,6));
+            //Console.WriteLine(groupName.ToLower());
+            //Console.WriteLine(groupName.ToUpper());
+            //Console.WriteLine(groupName.Substring(0,6));
 
 
 
@@ -84,7 +85,69 @@ namespace DataTypes
             ////_name.
             //    name.
 
-            dynamic age = 20;
+            //dynamic age = 20;
+
+            #endregion
+
+            #region typeConversion
+
+            #region parse
+
+            //string a = "123";
+
+            //int b = int.Parse(a);
+
+            //Console.WriteLine("b-nin deyeri :{0}",b*5);
+
+            ////string c = "IsDeleted";
+            //string c = "true";
+
+            //bool f = bool.Parse(c);
+            //Console.WriteLine("b-nin deyeri :{0}", f );
+            #endregion
+            #region convert
+
+            //string a = "500";
+
+            //int b = Convert.ToInt32(a);
+
+            //Console.WriteLine("b-nin deyeri {0}{1}",b,a);
+            //string p = "3,14";
+            //double v = Convert.ToDouble(p);
+
+            //#endregion
+
+            //#region impilict
+
+            //short b1 = 500;
+            //int b2 = b1;
+
+
+            #endregion
+            #region explicit
+
+            //int l = 5000;
+            //byte l1 = (byte) l;
+
+
+            #endregion
+
+            checked
+            {
+                int l = 5000;
+                byte l1 = (byte)l;
+            }
+            //int l = 5000;
+            //byte l1 = (byte)l;
+            //unchecked
+            //{
+            //    int l = 5000;
+            //    byte l1 = (byte)l;
+            //}
+            //string h = "true";
+            bool u = true;
+            int k = Convert.ToInt32(u);
+            char p = 'a';
 
             #endregion
         }
