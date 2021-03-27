11555fd2c3b9f69069beec89a03824b72e5f5483
parvizrovshanaliyev <parvizra@code.edu.az>
2021-03-27 14:36:45 +0400

    if else if

--- Week3.DecisionMaking/Program.cs
+++ Week3.DecisionMaking/Program.cs
@@ -6,8 +6,102 @@ namespace Week3.DecisionMaking
     {
         static void Main(string[] args)
         {
-            #region if statement
-            s
+            #region if else else if 
+            /*
+             * &&
+             * ||
+             *
+             * daxil edilen 2 deyeri toplayib 50 ile muqayise et
+             */
+
+            //Console.Write("enter a number 1: ");
+            ////string number1 = Console.ReadLine();
+            ////decimal number_ = Convert.ToDecimal(number1);
+
+            //decimal number1 = Convert.ToDecimal(Console.ReadLine());
+
+            //Console.Write("enter a number 2: ");
+            //decimal number2 = Convert.ToDecimal(Console.ReadLine());
+
+            //decimal total = number1 + number2;
+
+            //bool checkValue = total > 50;
+
+            //bool checkValue = (number1 + number2) > 50;
+
+            //if (checkValue)
+            //{
+            //    Console.WriteLine(" 50 boyukdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("50-den kicikdir");
+            //}
+
+            //if ((number1 + number2) > 50)
+            //{
+            //    Console.WriteLine(" 50 boyukdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("50-den kicikdir");
+            //}
+
+            //if ((number1 + number2) > 50)
+            //{
+            //    Console.WriteLine(" 50 boyukdur");
+            //}
+            //else if((number1 + number2) > 50 && (number1 + number2) >60)
+            //{
+            //    Console.WriteLine("50-den kicikdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("hecbirini odemedi");
+            //}
+
+            /*
+             * birden cox sertimiz uygn oldugu varaintlarda 
+             */
+
+            // 250
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            // xeta 
+            //if (a > 100 && a <= 200)
+            //{
+
+            //}else if (a > 200 && a <= 300)// true
+            //{
+            //    Console.WriteLine("200 -300");
+            //}else if (a > 200 && a <= 400)// true
+            //{
+            //    Console.WriteLine(" 200 -400 araligi");
+            //}
+            // duz varianti 
+            //if (a > 100 && a <= 200)
+            //{
+
+            //}
+
+            //if (a > 200 && a <= 300)
+            //{
+            //    Console.WriteLine("200 -300");
+            //}
+
+            //if (a > 200 && a <= 400)// true
+            //{
+            //    Console.WriteLine(" 200 -400 araligi");
+            //}
+
+            #region skopsuz istifade
+
+            //if (a > 200 && a <= 300)
+                //Console.WriteLine("200 -300");
+            //Console.WriteLine("200 -300");
+
+
+            #endregion
             #endregion
         }
     }
