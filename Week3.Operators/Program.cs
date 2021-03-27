fd04ac600d28ebde92c08448c32e4286ffc3db70
parvizrovshanaliyev <parvizra@code.edu.az>
2021-03-27 10:06:46 +0400

    operators region

--- Week3.Operators/Program.cs
+++ Week3.Operators/Program.cs
@@ -6,7 +6,99 @@ namespace Week3.Operators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region arithmetic
+            // +
+            // -
+            // *
+            // /
+            // %
+           
+
+            #endregion
+
+            #region relational
+            // iki deyer arasinda boyuk , kicik ve beraber olmasini
+            // munasibetini qarsilasdiran operatorlardir
+            // geriye boool tipi qaytarir
+            /*
+             * <
+             * >
+             * <=
+             * >=
+             * ==
+             */
+
+            #endregion
+
+            #region logical
+            /*
+             * &&  ve
+             * ||  ve ya
+             * ^   ya da
+             */
+
+            
+
+            #endregion
+
+            #region string ifadeler-de + , += , == opretatorlarinin istifadesi
+
+            
+
+
+            #endregion
+
+            #region deyil ! != beraber deyilse
+
+            #endregion
+
+            #region ternary
+
+            #endregion
+
+            #region member access . operatoru
+
+            #endregion
+
+            #region typeof operatoru
+           
+
+            #endregion
+
+            #region default operatoru
+            
+            #endregion
+
+            #region is operatoru
+           
+            #endregion
+
+            #region is null operatoru
+           
+            #endregion
+
+            #region is not null operatoru
+            
+            #endregion
+
+            #region as operatoru
+            
+            #endregion
+
+            #region ? (Nullable) Operatoru
+            
+            #endregion
+
+            #region ?? null coalescing operatoru
+            
+            #endregion
+
+            #region ??= null coalescing c# 8.0
+            
+            #endregion
+
+
+            Console.ReadLine();
         }
     }
 }
